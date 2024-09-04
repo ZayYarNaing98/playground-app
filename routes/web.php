1208c2f841896719f8a7c41f78a3a2895c3e3cc8
ZayYarNaing <zayyarnaing@aceplussolutions.com>
2024-09-05 02:01:35 +0630

    Add Laravel Routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Static Route
+Route::get('/blogs', function () {
+    return "Hello, This is blog page.";
+});
+
+
+// Dynamic Route
+Route::get('/blogs/{id}', function($id) {
+    return "Hello, This is blog detail - $id.";
+});
+
+
+// Routes Name
+Route::get('/dashboard', function(){
+    return "Welcome from TPP Program Dashboard!";
+})->name('dashboard.tpp');
+
+Route::get('/tpxp', function(){
+    return redirect()->route('dashboard.tpp');
+});
+
+
+// Group Routes
+Route::prefix('dashboard')->group(function() {
+    Route::get('/admin', function() {
+        return "This is admin dashboard.";
+    });
+
+    Route::get('/users', function() {
+        return "This is user dashboard.";
+    });
+});
+
+
+Route::get('/', function() {
+    return view('index');
 });
